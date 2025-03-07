c59a04df882130e124e0fd83a549e754a3e1af3c
kandukurissrkramesh <rameshkssrk@gmail.com>
2025-03-07 02:03:19 -0800

    copied

--- src/main/java/com/test/sb/controller/HomeController.java
+++ src/main/java/com/test/sb/controller/HomeController.java
@@ -6,10 +6,10 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 
-@RequestMapping("/")
+
 public class HomeController {
 
-	@GetMapping("/home")
+	@GetMapping("/")
 	public String hello() {
 		return new String("Hello world!");
 	}
