8b5a3b4a3ad0af05ccdcea5b50ec2da1b0b8c61b
kandukurissrkramesh <rameshkssrk@gmail.com>
2025-03-07 00:21:19 -0800

    first Commit

---
+++ src/main/java/com/test/sb/controller/HomeController.java
@@ -0,0 +1,16 @@
+package com.test.sb.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+@RequestMapping("/")
+public class HomeController {
+
+	@GetMapping("home")
+	public String hello() {
+		return new String("Hello world!");
+	}
+}
